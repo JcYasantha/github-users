3ec6a238696156f54ed608e23a3bf3a968bc6e4b
JcYasantha <yasantha619@gmail.com>
2020-08-30 16:22:17 +0530

    Updates

---
+++ precache-manifest.fc6d0c5ad787705b588f64262cd32ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b760e6e2197ad43a273495bd3297f070",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "845c0fd69ab5441054ff",
+    "url": "/github-users/static/css/main.b7328757.chunk.css"
+  },
+  {
+    "revision": "f9c5cd3d5cc174c2f7b4",
+    "url": "/github-users/static/js/2.03ff5f47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-users/static/js/2.03ff5f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845c0fd69ab5441054ff",
+    "url": "/github-users/static/js/main.3443f5a5.chunk.js"
+  },
+  {
+    "revision": "f453baa956893dd8728d",
+    "url": "/github-users/static/js/runtime-main.75e0f370.js"
+  }
+]);
